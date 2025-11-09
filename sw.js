512adc1fece176317c20a9841fef8919a5e3eea2
OuassVTC <ouassvtc@yahoo.com>
2025-11-09 10:13:25 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ouassvtc-cache-v2'; // <-- on change la version
+const CACHE_NAME = 'ouassvtc-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -8,26 +8,23 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
-    })
+    caches.keys().then((names) =>
+      Promise.all(
+        names.filter((name) => name !== CACHE_NAME)
+             .map((name) => caches.delete(name))
+      )
+    )
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
