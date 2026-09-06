d0fd8d31f6a771148dd8683c5a048f73c5e0088f
OuassVTC <ouassvtc@yahoo.com>
2026-09-06 07:11:35 +0200

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0DXbsmSkymE55YZfZytGa-newiXBw2lU",
+  authDomain: "ouassvtc-chauffeur-da466.firebaseapp.com",
+  projectId: "ouassvtc-chauffeur-da466",
+  storageBucket: "ouassvtc-chauffeur-da466.firebasestorage.app",
+  messagingSenderId: "394811166195",
+  appId: "1:394811166195:web:23da8a59731c22c6f51686"
+};
+
+export const firebaseReady = true;
