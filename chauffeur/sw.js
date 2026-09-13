bc896021a15885e5a9b17aff5dd22c38c117acba
OuassVTC <ouassvtc@yahoo.com>
2026-09-13 23:58:43 +0200

    sw.js

--- chauffeur/sw.js
+++ chauffeur/sw.js
@@ -29,7 +29,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
  
-const CACHE_NAME = "ouassvtc-chauffeur-v47";
+const CACHE_NAME = "ouassvtc-chauffeur-v48";
 const APP_SHELL = [
   "/chauffeur/",
   "/chauffeur/manifest.json",
